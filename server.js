0f0f7a9a247fdfc2eb7bc672d3c841ea8b591c82
IsrealOfficialTech <isrealabiolaofficial@gmail.com>
2025-08-28 18:55:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys");
-const fs = require("fs");const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const { makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys");
 const fs = require("fs");
 
 const app = express();
